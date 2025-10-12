c7029dc5e4aa81d1b593d9f46775ff596322cd40
uzumakibegatu@gmail.com <uzumakibegatu@gmail.com>
2025-10-12 11:35:48 +0700

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-a = int(input("math: "))
-b = int(input("english: "))
-c = int(input("literature: "))
-d = (a+b+c)/3
-
-if d > 8 and d <= 10:
-    print("gioi")
-elif d > 5 and d <= 8:
-    print("kha") 
-else:
-    print("kem")
+a = float(input("speed: "))
+b = float(input("time: "))
+c = a*b
+print("distance: ", c)
